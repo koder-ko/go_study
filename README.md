a904451bbd43fd1ffefd47f11f05c6685c34b92d
BuildTools <unconfigured@null.spigotmc.org>
2020-09-07 22:55:19 +0900

    [2020-09-07] making things right

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-#go study
+# go study
 
 this project is go tutorial for gopher beginners.
 
 [2020-09-07] fmt_use.go add
 
 [2020-09-07] make packages
+
+[2020-09-07] making things right
--- chapter1_begin/fmt_use.go
+++ chapter1_begin/fmt_use.go
@@ -15,7 +15,7 @@ func usePrint() {
 		c 언어의 프린트 방식 이용 printf
 	*/
 	fmt.Printf("%d is my number", 7)        // 7 is my number + 다음라인 전환x
-	fmt.Printf("go is the best = %b", true) // go is the best = true + 다음라인 전환 x
+	fmt.Printf("go is the best = %t", true) // go is the best = true + 다음라인 전환 x
 	// printf => print format 즉 뒤에 들어온 인자를 문자열로 포맷하여 넣어준다는 뜻
 
 	// %d, %b 는 서식문자로 꽤나 종류가 다양합니다.
@@ -32,9 +32,7 @@ func usePrint() {
 		%f	10진수 방식의 고정 소수점 실수
 		%F	10진수 방식의 고정 소수점 실수
 		%e	지수 표현 실수, e
-		%E
-
-		지수 표현 실수, E
+		%E  지수 표현 실수, E
 		%g	간단한 10진수 실수
 		%G	간단한 10진수 실수
 		%s	문자열
